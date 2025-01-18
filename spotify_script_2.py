696d7d099c0fd44ce0f03525b7073b08c30176b4
Dylan3220 <128821905+Dylan3220@users.noreply.github.com>
2025-01-18 13:19:15 -0500

    Update spotify_script_2.py
    commented out the monitor playback daemon thread, i think it was making too many requests and spotify api got angry and said this
    
    WARNING:root:Your application has reached a rate/request limit. Retry will occur after: 31661

--- spotify_script_2.py
+++ spotify_script_2.py
@@ -239,9 +239,9 @@ second_encoder.when_rotated_clockwise = update_forward_station
 second_encoder.when_rotated_counter_clockwise = update_backward_station
 switch.when_pressed = on_button_press
 
-playback_thread = threading.Thread(target=monitor_playback)
-playback_thread.daemon = True
-playback_thread.start()
+#playback_thread = threading.Thread(target=monitor_playback)
+#playback_thread.daemon = True
+#playback_thread.start()
 
 nfc_thread = threading.Thread(target=nfc_listener)
 nfc_thread.daemon = True
