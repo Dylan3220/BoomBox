719507ffc367dc81445cbf40d0728598fa243a5e
Dylan3220 <128821905+Dylan3220@users.noreply.github.com>
2025-01-22 21:40:20 -0500

    Update downtown.sh

--- downtown.sh
+++ downtown.sh
@@ -2,7 +2,7 @@
 sudo wget -O /home/diego/spotify_script_2.py https://raw.githubusercontent.com/Dylan3220/BoomBox/main/spotify_script_2.py
 sudo wget -O /etc/systemd/system/dylan_spotify.service https://raw.githubusercontent.com/Dylan3220/BoomBox/main/dylan_spotify.service 
 
-sudo systemctl daemon-reload
+##sudo systemctl daemon-reload
 
 # Enable and start the service
 sudo systemctl enable dylan_spotify.service
