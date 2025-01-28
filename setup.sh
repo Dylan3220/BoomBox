934a60d182aa9c18d3f7efa8810ba483aef12fab
Dylan3220 <128821905+Dylan3220@users.noreply.github.com>
2025-01-27 20:33:10 -0500

    Update and rename downtown.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,12 @@ sudo wget -O /home/diego/spotify_script_2.py https://raw.githubusercontent.com/D
 sudo wget -O /etc/systemd/system/dylan_spotify.service https://raw.githubusercontent.com/Dylan3220/BoomBox/main/dylan_spotify.service 
 sudo wget -O /etc/systemd/system/watchdog.service https://raw.githubusercontent.com/Dylan3220/BoomBox/main/watchdog.service
 sudo wget -O /home/diego/watchdog.sh https://raw.githubusercontent.com/Dylan3220/BoomBox/main/watchdog.sh
+sudo wget -O /boot/firmware/config.txt https://raw.githubusercontent.com/Dylan3220/BoomBox/main/config.txt
+sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh  
+sudo apt install python3-pip
+pip install spotipy --break-system-packages 
+pip install mfrc522 --break-system-packages
+
+sudo systemctl enable dylan_spotify.service
+sudo systemctl enable watchdog.service
+sudo systemctl daemon-reload
