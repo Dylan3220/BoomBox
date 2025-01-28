1c10da5f9120e9d61fdfbd161215ce83367ad305
Dylan3220 <128821905+Dylan3220@users.noreply.github.com>
2025-01-27 21:48:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo wget -O /home/diego/spotify_script_2.py https://raw.githubusercontent.com/D
 sudo wget -O /etc/systemd/system/dylan_spotify.service https://raw.githubusercontent.com/Dylan3220/BoomBox/main/dylan_spotify.service 
 sudo wget -O /etc/systemd/system/watchdog.service https://raw.githubusercontent.com/Dylan3220/BoomBox/main/watchdog.service
 sudo wget -O /home/diego/watchdog.sh https://raw.githubusercontent.com/Dylan3220/BoomBox/main/watchdog.sh
+sudo wget -O /home/diego/downtown.sh https://raw.githubusercontent.com/Dylan3220/BoomBox/main/downtown.sh
 sudo wget -O /boot/firmware/config.txt https://raw.githubusercontent.com/Dylan3220/BoomBox/main/config.txt
 sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh  
 sudo apt install python3-pip
