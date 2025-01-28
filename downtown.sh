67d78073fd16526366b337f23ad5a420971ca6d5
Dylan3220 <128821905+Dylan3220@users.noreply.github.com>
2025-01-27 19:15:35 -0500

    Update downtown.sh

--- downtown.sh
+++ downtown.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo wget -O /home/diego/spotify_script_2.py https://raw.githubusercontent.com/Dylan3220/BoomBox/main/spotify_script_2.py
 sudo wget -O /etc/systemd/system/dylan_spotify.service https://raw.githubusercontent.com/Dylan3220/BoomBox/main/dylan_spotify.service 
+sudo wget -O /etc/systemd/system/watchdog.service https://raw.githubusercontent.com/Dylan3220/BoomBox/main/watchdog.service
+sudo wget -O /home/diego/watchdog.sh https://raw.githubusercontent.com/Dylan3220/BoomBox/main/watchdog.sh
